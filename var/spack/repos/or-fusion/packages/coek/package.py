f243b06df37896e4703eaeed3e8ad2c494cfb9af
whart222 <whart222@gmail.com>
2024-04-19 05:52:09 -0600

    Build with asl and cppad

--- var/spack/repos/or-fusion/packages/coek/package.py
+++ var/spack/repos/or-fusion/packages/coek/package.py
@@ -29,13 +29,11 @@ class Coek(CMakePackage, PythonExtension):
     variant("verbose", default=False, description="Enable verbose build")
     variant("threads", default=True, description="Build with threads enabled")
     variant("shared", default=True, description="Build shared library")
-    variant("pic", default=True, description="Build position-independent code")
 
     variant("tests", default=False, description="Build coek test executables")
     variant("python", default=False, description="Build pycoek and install coek python libraries")
     variant("compact", default=False, description="Build compact expressions in coek")
 
-    variant("tpls", default=True, description="Build with all dependencies")
     variant("openmp", default=True, description="Build with openmp")
     variant("gurobi", default=False, description="Build with Gurobi optimization library")
     variant("cppad", default=False, description="Build with the CppAD library")
@@ -52,13 +50,11 @@ class Coek(CMakePackage, PythonExtension):
         extends("python")
     depends_on("fmt@8.0.1")
     depends_on("rapidjson@1.1.0")
-    #with when("+tpls"):
-    #    depends_on("cppad")
-    #    depends_on("asl")
-    #with when("+cppad"):
-    #    depends_on("cppad")
-    #with when("+asl"):
-    #    depends_on("asl")
+
+    with when("+cppad"):
+        depends_on("cppad@20240000.4:")
+    with when("+asl"):
+        depends_on("asl")
 
     def setup_run_environment(self, env):
         if self.spec.satisfies("+python"):
@@ -79,35 +75,45 @@ class Coek(CMakePackage, PythonExtension):
             args.append("-DBUILD_SHARED_LIBS=ON")
         elif self.spec.satisfies("-shared"):
             args.append("-DBUILD_SHARED_LIBS=OFF")
+
         if self.spec.satisfies("+debug"):
             args.append("-Dwith_debug=ON")
+
         if self.spec.satisfies("+verbose"):
             args.append("-Dwith_verbose=ON")
+
         if self.spec.satisfies("+threads"):
             args.append("-Dwith_threads=ON")
+
         if self.spec.satisfies("+openmp"):
             args.append("-Dwith_openmp=ON")
+
         if self.spec.satisfies("+tests"):
             args.append("-Dwith_tests=ON")
             args.append("-Dwith_catch2=ON")
+
         if self.spec.satisfies("+python"):
             args.append("-Dwith_python=ON")
             args.append("-Dwith_pybind11=ON")
+
         if self.spec.satisfies("+compact"):
             args.append("-Dwith_compact=ON")
 
+        if self.spec.satisfies("+asl"):
+            args.append("-Dwith_asl=ON")
+
+        if self.spec.satisfies("+cppad"):
+            args.append("-Dwith_cppad=ON")
         #
         # External gurobi dependency found by CMAKE, not spack
         #
         if self.spec.satisfies("+gurobi"):
             args.append("-Dwith_gurobi=ON")
-
+        #
         # Require standard at configure time to guarantee the
         # compiler supports the selected standard.
+        #
         args.append("-DCMAKE_CXX_STANDARD_REQUIRED=ON")
         args.append("-DCMAKE_CXX_STANDARD={0}".format(spec.variants["cxxstd"].value))
-
-        ## Don't build tests
-        #args.append(self.define("FMT_TEST", self.run_tests))
 
         return args
